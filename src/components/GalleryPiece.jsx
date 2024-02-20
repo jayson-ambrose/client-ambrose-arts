48cd6c57e9375d7bb766bc39b3badcc635b04013
jayson ambrose <jayson.ambrose.87@gmail.com>
2024-02-19 21:53:56 -0700

    experimenting with the draggable=false

--- src/components/GalleryPiece.jsx
+++ src/components/GalleryPiece.jsx
@@ -12,7 +12,7 @@ export default function GalleryPiece ({ piece }) {
     return (
         <div>
             <div key={piece.id}>
-                <img onClick={() => handleSetGalleryModal()} className={'block h-96 w-auto'} src={piece.img_url}/>
+                <img draggable='false' onClick={() => handleSetGalleryModal()} className={'block h-96 w-auto'} src={piece.img_url}/>
                 <h2 className={`text-2xl px-8 pt-2 font-advent`}>{piece.title}</h2>
                 <GalleryModal galleryModal={galleryModal} setGalleryModal={setGalleryModal} piece={piece}/>
             </div>
