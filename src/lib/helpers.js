68bd2db6a1436edb43a86a8a18a2b86ab122df17
jayson ambrose <jayson.ambrose.87@gmail.com>
2024-06-03 10:57:10 -0600

    test

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -3,5 +3,5 @@
 
 // export const urlPrefix = '/api' //proxy
 // export const urlPrefix = 'http://127.0.0.1:5555' //dev server
-export const urlPrefix = 'https://server-ambrose-art.onrender.com' //live server
+// export const urlPrefix = 'https://server-ambrose-art.onrender.com' //live server
 
