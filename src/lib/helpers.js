3f91258c198a3d6880b85888047acce7d8cea019
jayson ambrose <jayson.ambrose.87@gmail.com>
2024-06-03 10:52:57 -0600

    test

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,7 +1,7 @@
 // urlPrefix is imported wherever there is a fetch. Use this file for testing api endpoints
 
 
-// export const urlPrefix = '/api' //proxy
+export const urlPrefix = '/api' //proxy
 // export const urlPrefix = 'http://127.0.0.1:5555' //dev server
-export const urlPrefix = 'https://server-ambrose-art.onrender.com' //live server
+// export const urlPrefix = 'https://server-ambrose-art.onrender.com' //live server
 
